4b459f02ed12c6dfc0319c98085697c3af1899ea
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-01 02:48:16 +0100

    chore(starter-file): define-entry-file

---
+++ server/Server.js
@@ -0,0 +1,19 @@
+/**
+ * Entry point for Document Manangement System
+ */
+import http from 'http';
+import * as dotenv from 'dotenv';
+import logger from 'js-logger';
+import app from './server/config/App';
+
+const port = process.env.PORT || 3000;
+dotenv.config({
+  silent: true
+});
+
+logger.useDefaults();
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+logger.info(`App is running at: ${port}/`);
