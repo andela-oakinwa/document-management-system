74168d5252a126e2b9aecaecab41ca41c70bbf94
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-01 18:03:34 +0100

    chore(webpack):webpack-reconfigured

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './server/Server.js',
+  entry: ['./server/Server.js'],
   output: { path: __dirname, filename: './client/public/build/Bundle.js' },
   watch: true,
   module: {
