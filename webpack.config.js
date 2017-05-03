feb794b4300de244a3b5f03f3ab80b1feb6d0980
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-03 18:04:37 +0100

    chore(webpack-configured)

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,38 @@
-const path = require('path');
+/**
+ * Dependencies declared
+ * @type {Object}
+ */
+const path = require('path'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: ['./server/Server.js'],
+  entry: ['./server/Server.js', './client/src/assets/css/Index.scss'],
   output: { path: __dirname, filename: './client/public/build/Bundle.js' },
   watch: true,
+  devServer: {
+    contentBase: 'client/public/'
+  },
   module: {
     rules: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
         use: [
-          { loader: 'babel-loader' },
-          { loader: 'eslint-loader' }
+          { loader: 'babel-loader!eslint-loader' }
         ]
       },
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader',
-            options: { modules: true }
-          }
+          { loader: 'style-loader!css-loader' }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader!css-loader!sass-loader' }
         ]
       }
     ]
