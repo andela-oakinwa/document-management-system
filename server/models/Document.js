5f534532eb2d5c88bab13d6b039271080eac9bc6
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-04 13:23:10 +0100

    chore(model):define-model -Document model defined

--- server/models/Document.js
+++ server/models/Document.js
@@ -1,3 +1,60 @@
 /**
- * Routes for documents
+ * Document model
  */
+module.exports = (sequelize, DataTypes) => {
+  const Document = sequelize.define('Document', {
+    documentId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          message: 'This field cannot be empty.'
+        }
+      }
+    },
+    ownerId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    access: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'public',
+      validate: {
+        notEmpty: {
+          message: 'This field cannot be empty.'
+        },
+        isIn: {
+          values: [['public', 'private', 'role']],
+          message: 'public, private or role required'
+        },
+      }
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          message: 'This field cannot be empty.'
+        }
+      }
+    },
+    ownerRoleId: {
+      type: DataTypes.STRING
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        Document.belongsTo(models.User, {
+          foreignKey: 'ownerId',
+          onDelete: 'CASCADE',
+        });
+      }
+    }
+  });
+  return Document;
+};
