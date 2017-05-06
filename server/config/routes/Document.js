49d65d8422a9fe877333ba40a3441fb679653c6b
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-06 12:17:07 +0100

    chore(structure):re-structure-project-folder

--- server/config/routes/Document.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import authenticate from '../middlewares/Authenticate';
-
-const documentRouter = express.Router();
-
-export default documentRouter;
--- server/config/routes/User.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import authenticate from '../middlewares/Authenticate';
-
-const userRouter = express.Router();
-
-export default userRouter;
