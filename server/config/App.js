34785f14db02348fd55d71a2211ee385559fc149
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-12 13:37:52 +0100

    chore(entry-file):modify-default-route

--- server/config/App.js
+++ server/config/App.js
@@ -4,29 +4,38 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import userRouter from './UserRouter';
-import roleRouter from './RoleRouter';
-import documentRouter from './DocumentRouter';
-
-
-// Create an instance of the express app
+import userRouter from '../routes/UserRouter';
+import roleRouter from '../routes/RoleRouter';
+import documentRouter from '../routes/DocumentRouter';
+/**
+ * Create an instance of the express app
+ * @type {Object}
+ */
 const app = express();
-
-// Disables logging sensitive
+/**
+ * Disables logging server information
+ */
 app.disable('x-powered-by');
 
-// Log requests to the console.
-app.use(logger('Dev'));
-
-// Parse incoming requests data, this will happen on every request
+/**
+ * Console logs for development purpose.
+ */
+app.use(logger('dev'));
+/**
+ * Parse incoming requests data, this will happen on every request
+ */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+/**
+ * Homepage route
+ * @param  {Object} '/'Root url
+ * @param  {Function} Callback
+ * @return {Object}
+ */
 app.get('/', (request, response) => {
   response.status(200)
   .send({ message: 'Welcome to Doqman Document Management System' });
 });
-
 app.use('/users', userRouter);
 app.use('/documents', documentRouter);
 app.use('/roles', roleRouter);
