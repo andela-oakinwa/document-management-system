0039e1820d811ca8a1333fdcef6b490ee81d73ce
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-17 20:27:40 +0100

    chore(express-file):add-hot-reload

--- server/config/App.js
+++ server/config/App.js
@@ -1,10 +1,15 @@
 /**
- * Dependencies called
+ * Dependencies declared
  */
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+import webpackConfig from '../../webpack.config';
 import userRouter from '../routes/UserRouter';
 import roleRouter from '../routes/RoleRouter';
 import documentRouter from '../routes/DocumentRouter';
@@ -12,37 +17,37 @@ import documentRouter from '../routes/DocumentRouter';
  * Create an instance of the express app
  * @type {Object}
  */
-const app = express();
-/**
- * Disables logging server information
- */
-app.disable('x-powered-by');
-
-/**
- * Console logs for development purpose.
- */
-app.use(logger('dev'));
+const app = express(),
+  compiler = webpack(webpackConfig);
+app.use(express.static(path.join(__dirname, '../../client/')));
 /**
  * Parse incoming requests data, this will happen on every request
  */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 /**
- * Homepage route
- * @param  {Object} '/'Root url
- * @param  {Function} Callback
- * @return {Object}
+ * Disables logging server information
  */
-app.get('/', (request, response) => {
-  response.status(200)
-  .sendFile(path.join(__dirname, '../../client/public/Index.html'));
-});
+app.disable('x-powered-by');
+app.use(logger('dev'));
+/**
+ * Hot reload
+ */
+app.use(webpackMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+/**
+ * Routes
+ */
 app.use('/users', userRouter);
 app.use('/documents', documentRouter);
 app.use('/roles', roleRouter);
-
 app.get('*', (request, response) => {
-  response.status(404).send({ message: 'REQUEST PAGE NOT FOUND.' });
+  response.status(200)
+    .sendFile(path.join(__dirname, '../../client/Index.html'));
 });
 
 export default app;
