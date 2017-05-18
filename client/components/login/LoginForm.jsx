f13c23dcee910f9cbef28055265e09da544a1b4a
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-18 02:09:54 +0100

    feature(ajax-calls):implement-ajax-calls-via-thunk

--- client/components/login/LoginForm.jsx
+++ client/components/login/LoginForm.jsx
@@ -1,51 +1,10 @@
 import React from 'react';
 import { Row, Input, Button } from 'react-materialize';
-/**
- * Login form 
- * @param  {Object} options.errors     
- * @param  {Object} options.onChange   
- * @param  {Object} options.loginProps 
- * @param  {Object} options.onSubmit   
- * @return {Object}                    
- */
+
 const LoginForm = ({ errors, onChange, loginProps, onSubmit }) => {
   return (
-    <form onSubmit={onSubmit}>
-      <h1>Login</h1>
-      <Row>
-        {errors.form && <div style={{ color: '#BD1D11' }}>{errors.form}</div>}
-        <Input
-            label="Username or Email"
-            s={12}
-            placeholder="Username or Email"
-            onChange={onChange}
-            value={loginProps.identifier}
-            name="identifier"
-            id="identifier"
-            />
-        <Input
-            label="Password"
-            s={12}
-            placeholder="Password"
-            onChange={onChange}
-            value={loginProps.password}
-            type="password"
-            name="password"
-            id="password"
-            />
-        <Input
-            id="loginButton" value="Login"
-            className="btn waves-effect waves-light" name="action" type="submit" />
-      </Row>
-    </form>
+    <input />
   );
-};
-
-LoginForm.propTypes = {
-  errors: React.PropTypes.object.isRequired,
-  onChange: React.PropTypes.func.isRequired,
-  loginProps: React.PropTypes.object.isRequired,
-  onSubmit: React.PropTypes.func.isRequired,
 };
 
 export default LoginForm;
