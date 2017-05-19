07900a0e0b4c0717c7e312fbff83510131d265eb
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-05-19 22:59:52 +0100

    feature(landing-page):add-landing-page-component

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -4,13 +4,17 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Greetings from './components/Greetings'; 
 import NavigationBar from './components/navigation_bar/NavigationBar';
-import SignupPage from './components/signup/SignupPage';
+import SignUpPage from './components/signup/SignUpPage';
+import About from './components/About';
+import Login from './components/login/LoginPage';
 
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Greetings} />
-    <Route path='/signup' component={SignupPage} />
+    <Route path='/signup' component={SignUpPage} />
+    <Route path='/about' component={About} />
+    <Route path='/login' component={Login} />
   </Route>
 );
 
