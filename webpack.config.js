71f4572f27492857f51a0ff1f5496003d227bff3
Oluwafemi Akinwa <andela-oakinwa@users.noreply.github.com>
2017-05-31 16:19:36 +0100

    chore(webpack):add-babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ import webpack from 'webpack';
 export default {
   devtool: 'eval-source-map',
   entry: [
+    'babel-polyfill',
     'webpack-hot-middleware/client',
     path.join(__dirname, '/client/Index.jsx')
     ],
