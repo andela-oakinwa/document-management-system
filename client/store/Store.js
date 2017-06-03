1329a3c2431cc3685edf958e755d71a9b7dcffc3
Oluwafemi Akinwa <andela-oakinwa@users.noreply.github.com>
2017-06-03 15:42:43 +0100

    chore(store):modify-&-refactor-store

--- client/store/Store.js
+++ client/store/Store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import rootReducer from '../reducers/RootReducer';
 
-export default(initialState) => {
-  return createStore(rootReducer, initialState);
+const Store = (initialState) => {
+  return createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(thunk, reduxImmutableStateInvariant()),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+  );
 };
+
+export default Store;
