7de501fda88deb6436e4a2bf58e4f67028b7b2e4
Oluwafemi Akinwa <andela-oakinwa@users.noreply.github.com>
2017-06-05 01:50:37 +0100

    chore(test):add-component-tests

---
+++ client/test/component/LoginForm.test.js
@@ -0,0 +1,32 @@
+import expect from 'expect';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import LoginForm from '../../components/login/LoginForm';
+
+function setup() {
+  const props = {
+    errors: {},
+    loginProps: {},
+    onSubmit: () => {},
+    onChange: () => {}
+  };
+
+  return mount(<LoginForm {...props} />);
+}
+
+describe('Component: LoginForm', () => {
+  it('renders form and h5', () => {
+    const wrapper = setup();
+    expect(wrapper.find('form')).toExist;
+    expect(wrapper.find('h5').text()).toEqual('Please login into your account.');
+    expect(wrapper.find('Row')).toExist;
+    expect(wrapper.find('div')).toExist;
+  });
+  it('should take props', () => {
+    const wrapper = setup();
+    expect(wrapper.props().errors).toExist;
+    expect(wrapper.props().loginProps).toExist;
+    expect(wrapper.props().onChange).toExist;
+    expect(wrapper.props().onSubmit).toExist;
+  });
+});
