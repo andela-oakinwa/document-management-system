157dd37e2cabf1128009515afdf9db30016678f7
Oluwafemi Akinwa <andela-oakinwa@users.noreply.github.com>
2017-06-06 20:14:08 +0100

    chore(heroku):fix-deployment-error

--- client/components/login/LoginForm.jsx
+++ client/components/login/LoginForm.jsx
@@ -21,6 +21,7 @@ const LoginForm = ({ errors, onChange, loginProps, onSubmit }) => {
               onChange={onChange}
               value={loginProps.email}
               name="email"
+              type="email"
               id="email"
               className="validate"
             />
@@ -40,6 +41,7 @@ const LoginForm = ({ errors, onChange, loginProps, onSubmit }) => {
             <i className="small material-icons">lock</i>
             <div className="center">
               <Button
+                id="login"
                 type="submit"
                 className="col s12 btn blue btn-large login-btn waves-effect"
               >
