1fe1e6e58c98acb7fff925b51f1331ef986b1020
Oluwafemi Akinwa <andela-oakinwa@users.noreply.github.com>
2017-06-11 16:33:10 +0100

    chore

--- server/controllers/RoleController.js
+++ server/controllers/RoleController.js
@@ -14,6 +14,7 @@ const RoleController = {
    * @param {Object} response Response object
    */
   createRole(request, response) {
+    console.log(request.body);
     db.Role
       .create(request.body)
       .then((role) => {
